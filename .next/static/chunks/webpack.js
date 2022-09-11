9be2cc119096e03132aae85af090e496868e436d
Eldora Boo <eldoraboo.mby@gmail.com>
2022-09-12 04:17:09 +0800

    fix: change secret

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "87498fe37aca180ae4da"; }
+/******/ 		__webpack_require__.h = function() { return "ae9c3b2d8c9c6f7b06fa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     reactStrictMode: true,
     serverRuntimeConfig: {
-        secret: 'THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING'
+        secret: 'uHtch0XTy0meAIrbbtafjQ'
     },
     publicRuntimeConfig: {
         apiUrl: process.env.NODE_ENV === 'development'
